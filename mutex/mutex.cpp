d5d9987095b758fbad925e6ed813f9f35fc0e0c6
shivam dhir <shivamdhir90@gmail.com>
2026-01-12 17:09:33 -0800

    lock_guard

--- mutex/mutex.cpp
+++ mutex/mutex.cpp
@@ -9,6 +9,41 @@ std::mutex gLock;
 
 static int count = 0;
 
+void count_increment_lock_guard() {
+
+	// std::lock_guard is an RAII based locking mechanism, which returns the lock itself
+	std::lock_guard<std::mutex> lockGuard(gLock);
+	count++;
+
+	// when the scope ends and the lockGuard object is destroyed, its destructor will itseld reuturn the lock
+	// which avoids the possibility of a deadlock occurring.
+}
+
+void deadlock() {
+	gLock.lock();
+	count++;
+
+	// If we forget to unlock the mutex, the first thread that gets the lock will never return it adn all the other threads will wait 
+	// to acquire the lock, and the program becomes a deadlock and goes on forever.
+	// This is dangerous. To prevent this, we use lock_guard(), which is a RAII based lock
+	//gLock.unlock();
+
+	// This is another of handling a deadlock. If an exception occurs and the function doesn't return the lock, it results in a deadlock.
+	// we then have to handle the exception, return the lock, and again write unlock() if exception is not thrown
+	try {
+		count++;
+		throw "dangerous";
+	}
+	catch (...) {
+		gLock.unlock();
+		std::cout << "exception\n";
+		return;
+	}
+
+	gLock.unlock();
+
+}
+
 void count_increment() {
 	// the code between the lock() and unlock() is a critical section and can't be accessed by multiple
 	// threads at the same time. This makes the count++ an atomic section
